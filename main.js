5451a8def695a92ac7504b3185806eee13ec2221
Simon <62551180+simonagleev@users.noreply.github.com>
2022-06-20 18:52:38 +0300

    tablet css added + some fixes

--- main.js
+++ main.js
@@ -1,31 +1,83 @@
 let roadmapAnimItems = document.querySelectorAll(".roadmap_list_item");
+let meowverseVideos = document.querySelectorAll(".meowverse_video");
 
-const offset = (el) => {
-    const rect = el.getBoundingClientRect(),
-    scrollLeft = window.scrollX || document.documentElement.scrollLeft,
-    scrollTop = window.scrollY || document.documentElement.scrollTop;
-    return {top: rect.top + scrollTop, left: rect.left + scrollLeft}
-}
-
-if (roadmapAnimItems.length > 0) {
-    const animationOnScroll = () => {
-        for (let i = 0; i < roadmapAnimItems.length; i++) {
-            const item = roadmapAnimItems[i];
-            const itemHeight = item.offsetHeight;
-            const itemOffset = offset(item).top;
-            const animationStart = 4;
-
-            let itemPoint = window.innerHeight - itemHeight / animationStart;
-
-            if (itemHeight > window.innerHeight) {
-                itemPoint = window.innerHeight - window.innerHeight / animationStart;
-            }
-
-            if ((scrollY > itemOffset - itemPoint) && scrollY < (itemOffset + itemHeight)) {
-                item.classList.add("roadmap_list_item_active");
-            } else {
-                item.classList.remove("roadmap_list_item_active");
-            }
+function onEntry(entry) {
+    entry.forEach(change => {
+        if(change.isIntersecting) {
+            change.target.classList.add('roadmap_list_item_active')
         }
-    }
+    });
 }
+
+const options= {
+    threshhold: [0.5]
+}
+const observer = new IntersectionObserver(onEntry, options);
+
+for (let el of roadmapAnimItems) {
+    observer.observe(el)
+    console.log(el)
+}
+
+
+function onEntry(entry) {
+    entry.forEach(change => {
+        if(change.isIntersecting) {
+            change.target.classList.add('show')
+        }
+    });
+}
+
+for (let el of meowverseVideos) {
+    observer.observe(el)
+    console.log(el)
+}
+
+
+const menuCheckbox = document.getElementById("menu__toggle");
+
+const link= document.querySelectorAll('.menu__item')
+const menu= document.querySelector('.menu__box')
+const headerBurger = document.querySelector('.header__burger');
+const headerMenu = document.querySelector('.header__menu');
+const body = document.querySelector('.body');
+
+const navLinks = document.querySelectorAll('.menu__item');
+
+const test = () => {
+    // menuCheckbox.value = 'off'
+  console.log('heheh')
+}
+menu.addEventListener('click', function(){
+    console.log('heheh')
+    menuCheckbox.value = 'off'
+    console.log(menuCheckbox.labels)
+})
+console.log(menu)
+// link.addEventListener('click', test())
+
+// function doActiveMenu(evt) {
+//     evt.preventDefault();
+//     headerBurger.classList.toggle('active');
+//     headerMenu.classList.toggle('active');
+//     body.classList.toggle('lock');
+// }
+
+
+// navLinks.forEach((element) => {
+//     element.addEventListener('click', doActiveMenu)
+// })
+
+
+// headerBurger.addEventListener('click', doActiveMenu);
+
+
+
+var burgerMenu = document.getElementById('burger-menu');
+
+var overlay = document.getElementById('menu');
+
+burgerMenu.addEventListener('click', function() {
+  this.classList.toggle("close");
+  overlay.classList.toggle("overlay");
+});
